7b3976f598f5c214e0450d1522f406c4e58f534b
thescott <scott.mabe@me.com>
2021-04-03 20:32:29 -0400

    getting weird with it

--- variables.tf
+++ variables.tf
@@ -10,15 +10,6 @@ variable "location" {
 variable "project_id" {
     description = "The Project to launch into"
 }
-variable "network_name" {
-  description = "Creates name for Network and Cluster"
-}
 variable "name" {
     description = "Setting a name for resources to use"
-}
-variable "subnet_cidr1" {
-    description = "This will create a cidr range that you can define in the tf vars file"
-}
-variable "subnet_cidr2" {
-    description = "This will create an additional cidr range that you can define in the tf vars file"
 }
